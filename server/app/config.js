5678fc73847a8ddf6015204443c7f260608de097
Samuel Cha <sunchipssam@gmail.com>
2019-02-09 00:52:29 -0600

    add helmet to app config

--- server/app/config.js
+++ server/app/config.js
@@ -1,7 +1,9 @@
 const configs = [];
 
 if (process.env.NODE_ENV === 'development') {
-    configs.push(require('morgan')('combined'));
+  configs.push(require('morgan')('combined'));
 }
+
+configs.push(require('helmet')());
 
 module.exports = configs;
