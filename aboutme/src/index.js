13c03fb3ad4eb35e2ab384c503f1bf8592fb4d3c
Samuel Cha <sunchipssam@gmail.com>
2019-07-08 00:01:00 -0500

    add client code

---
+++ aboutme/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import App from './App';
+import './index.scss';
+
+import { ScrollToTop } from './components';
+
+ReactDOM.render(
+  <Router>
+    <ScrollToTop>
+      <App />
+    </ScrollToTop>
+  </Router>,
+  document.getElementById('root'),
+);
